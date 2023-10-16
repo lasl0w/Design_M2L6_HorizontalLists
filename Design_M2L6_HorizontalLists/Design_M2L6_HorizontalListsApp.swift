e18a506fa68e4c4ef83cc4377f4b811c0624bcf9
Tom Montgomery <montgomery3681@gmail.com>
2023-10-15 22:23:37 -0600

    Initial Commit

---
+++ Design_M2L6_HorizontalLists/Design_M2L6_HorizontalListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_M2L6_HorizontalListsApp.swift
+//  Design_M2L6_HorizontalLists
+//
+//  Created by tom montgomery on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Design_M2L6_HorizontalListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
